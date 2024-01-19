0f766bdeef0d69e556c4d2d7a89c6ca31972b6fd
anhsausd <anh.vu@sausd.us>
2024-01-19 14:15:36 -0800

    Added a console comment

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+//just anh test
+Console.WriteLine("Yo, good moring. It's Monday. Need coffee badly.")
